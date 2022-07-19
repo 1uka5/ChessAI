b70f0a76cba131a6677c940483df7362143a2ed0
hoodjustin <justinnerhood@yahoo.com>
2022-07-18 20:43:14 -0700

    Added display. No legal move checks yet

--- chess_main.py
+++ chess_main.py
@@ -3,13 +3,148 @@ TODO: Probably need to split up class declarations and main into separate files
 TODO: Implement castling
 TODO: Implement en passant
 '''
+from platform import java_ver
+import pygame
+
+from pygame.locals import (
+    MOUSEBUTTONUP,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+ 
+pygame.init()
+X = 400
+Y = 400
+display_surface = pygame.display.set_mode((X, Y))
+ 
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 700
+ 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+class Square:
+    def __init__(self, x_start, y_start, width_height, color):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.width_height = width_height
+        self.color = color
+        self.image = 0
+
+class PieceImage:
+    def __init__(self, x_start, y_start, width_height):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.width_height = width_height
+        self.image = 0
+        self.piece = 0
+        self.color = -1
+
+    def draw(self, x, y, piece, color):
+        self.color = color
+        if piece == 1:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitepawn.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackpawn.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+        if piece == 2:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whiterook.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackrook.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+        if piece == 3:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitehorse.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackhorse.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+        if piece == 4:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitebishop.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackbishop.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+        if piece == 5:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitequeen.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackqueen.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+        if piece == 6:
+            if color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whiteking.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackking.png")
+                self.image = pygame.transform.scale(self.image, (self.width_height - 5, self.width_height))  
+        if self.image != 0:
+            screen.blit(self.image, (x, y))
+        self.piece = piece
+
+    def erase(self, color):
+        surf = pygame.Surface((self.width_height, self.width_height))
+        print(self.width_height)
+        if color == 0:
+            print("a")
+            surf.fill((118, 150, 86))
+            rect = surf.get_rect()
+            screen.blit(surf, (self.x_start, self.y_start))
+            pygame.display.flip()
+        else:
+            print("b")
+            surf.fill((255,255,255))
+            rect = surf.get_rect()
+            screen.blit(surf, (self.x_start, self.y_start))
+            pygame.display.flip()
+        self.piece = 0
+
+        return color
+
 
 class Piece():
-    def __init__(self, role = 0, color = 0):
+    def __init__(self, role = 0, color = 0): 
         self.role = role
         self.color = color
         self.new = True # for pawn double movement and castling
         self.age = 0 # for en passant
+        self.image = 0
+        if self.role == 1:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitepawn.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackpawn.png")
+        if self.role == 2:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whiterook.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackrook.png")
+        if self.role == 3:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitehorse.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackhorse.png")
+        if self.role == 4:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitebishop.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackbishop.png")
+        if self.role == 5:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whitequeen.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackqueen.png")
+        if self.role == 6:
+            if self.color == 0:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/whiteking.png")
+            else:
+                self.image = pygame.image.load("C:/Users/justi/code/ChessAI/assets/blackking.png")
 
 '''
 ROLES:
@@ -238,8 +373,191 @@ class Board():
         self.staleCheckmate(color)
 
 
+def calculate_coordinates(x_array, y_array, color):
+    if SCREEN_WIDTH <= SCREEN_HEIGHT:
+        width_height = SCREEN_WIDTH / 8
+    else:
+        width_height = SCREEN_HEIGHT / 8
+    
+    x_coordinate = x_array * width_height
+    y_coordinate = y_array * width_height
+
+    return Square(x_coordinate, y_coordinate, width_height, color)
+
+def calculate_image_coordinates(x_array, y_array):
+    if SCREEN_WIDTH <= SCREEN_HEIGHT:
+        width_height = SCREEN_WIDTH / 8
+    else:
+        width_height = SCREEN_HEIGHT / 8
+    
+    x_coordinate = x_array * width_height
+    y_coordinate = y_array * width_height
+
+    return PieceImage(x_coordinate, y_coordinate, width_height)
+
+
+
 def main(): # remember to deal with king in check
     test = Board()
+
+    chess_board = []
+    pieces = []
+    color = 1 # 1 is black
+    for i in range(8):
+        chess_row = []
+        color = not color
+        for j in range(8):
+            chess_row.append(calculate_coordinates(i, j, color))
+            color = not color
+        chess_board.append(chess_row)
+
+    for i in range(8):
+        pieces_row = []
+        for j in range(8):
+            if j == 0:
+                color = 1
+                if i == 0 or i == 7:
+                    pieces_row.append(calculate_image_coordinates(i, j)) 
+                elif i == 1 or i == 6:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 2 or i == 5:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 3:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 4:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+            elif j == 1:
+                color = 1
+                pieces_row.append(calculate_image_coordinates(i, j))
+            elif j == 6:
+                color = 0
+                pieces_row.append(calculate_image_coordinates(i, j))
+            elif j == 7:
+                color = 0
+                if i == 0 or i == 7:
+                    pieces_row.append(calculate_image_coordinates(i, j)) 
+                elif i == 1 or i == 6:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 2 or i == 5:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 3:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+                elif i == 4:
+                    pieces_row.append(calculate_image_coordinates(i, j))
+            else:
+                pieces_row.append(calculate_image_coordinates(i, j))
+        pieces.append(pieces_row)
+
+    
+    for row in chess_board:
+        for square in row:
+            surf = pygame.Surface((square.width_height, square.width_height))
+    
+            if square.color == 1:
+                surf.fill((255, 255, 255))
+            else:
+                surf.fill((118, 150, 86))
+
+            #display_surface.blit(square.image, (square.x_start, square.y_start))
+    
+            rect = surf.get_rect()
+            screen.blit(surf, (square.x_start, square.y_start))
+            pygame.display.flip()
+
+    def get_square_for_position(pos):
+        for row in chess_board:
+            if row[0].y_start < pos[1] < row[0].y_start + row[0].width_height:
+                for square in row:
+                    if square.x_start < pos[0] < square.x_start + square.width_height:
+                        return square
+
+    # pieces = []
+    # for i in range(8):
+    #     for j in range(8):
+            
+    for i in range(8):
+        for j in range(8):
+            if j == 0:
+                color = 1
+                if i == 0 or i == 7:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 2, color) 
+                elif i == 1 or i == 6:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 3, color)
+                elif i == 2 or i == 5:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 4, color)
+                elif i == 3:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 5, color)
+                elif i == 4:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 6, color)
+            elif j == 1:
+                color = 1
+                pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 1, color)
+            elif j == 6:
+                color = 0
+                pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 1, color)
+            elif j == 7:
+                color = 0
+                if i == 0 or i == 7:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 2, color)
+                elif i == 1 or i == 6:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 3, color)
+                elif i == 2 or i == 5:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 4, color)
+                elif i == 3:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 5, color)
+                elif i == 4:
+                    pieces[i][j].draw(pieces[i][j].x_start, pieces[i][j].y_start, 6, color)
+
+
+
+    running = True
+    piece_clicked = False
+    original_pos = [-1, -1]
+    prev_piece = -1
+    while running:
+        for event in pygame.event.get():
+            #display_surface.blit(chess_board[0][0].image, (chess_board[0][0].x_start, chess_board[0][0].y_start))
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+    
+            if event.type == MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                pos = (int(pos[0] // 87.5), int(pos[1] // 87.5))
+                print((pos))
+                if not piece_clicked and pieces[pos[0]][pos[1]].piece != 0 and pieces[pos[0]][pos[1]].color == 0:
+                    piece_clicked = True
+                    print("Piece clicked")
+                    piece_clicked = pos
+                    print(len(pieces))
+                    print(len(pieces[0]))
+                    original_pos = pos
+                    prev_piece = pieces[pos[0]][pos[1]].piece
+                    prev_color = pieces[pos[0]][pos[1]].color
+                    surf_h = pygame.Surface((pieces[pos[0]][pos[1]].width_height, 5))
+                    surf_v = pygame.Surface((5, pieces[pos[0]][pos[1]].width_height))
+                    surf_h.fill((255, 0, 0))
+                    surf_v.fill((255, 0, 0))
+                    rect = surf.get_rect()
+                    screen.blit(surf_v, (pieces[pos[0]][pos[1]].x_start, pieces[pos[0]][pos[1]].y_start))
+                    screen.blit(surf_v, (pieces[pos[0]][pos[1]].x_start + pieces[pos[0]][pos[1]].width_height - 5, pieces[pos[0]][pos[1]].y_start))
+                    screen.blit(surf_h, (pieces[pos[0]][pos[1]].x_start, pieces[pos[0]][pos[1]].y_start + pieces[pos[0]][pos[1]].width_height - 5))
+                    screen.blit(surf_h, (pieces[pos[0]][pos[1]].x_start, pieces[pos[0]][pos[1]].y_start))
+                    pygame.display.flip()
+                elif piece_clicked: 
+                    print("Piece moved")
+                    piece_clicked = False
+                    print(original_pos)
+                    print(chess_board[original_pos[0]][original_pos[1]].color)
+                    color = pieces[original_pos[0]][original_pos[1]].erase(chess_board[original_pos[0]][original_pos[1]].color)
+                    pieces[pos[0]][pos[1]].draw(pieces[pos[0]][pos[1]].x_start, pieces[pos[0]][pos[1]].y_start, prev_piece, prev_color)
+
+
+            elif event.type == QUIT:
+                running = False
+                pygame.quit()
+
+            pygame.display.update() 
 
     while test.win == 0:
         test.printBoard()
@@ -253,7 +571,7 @@ def main(): # remember to deal with king in check
     winner = 1 if test.win == 2 else (2 if test.win == 1 else 0)
     
     if winner != 0:
-        print(f"Player {winner} has won! Congrats!")
+        print("Player {winner} has won! Congrats!")
 
 if __name__ == "__main__":
     main()
